3827d780c740cfb37d45d1c37a0644c7f715fa86
Stefan Noack <stefan@noack-software.de>
2013-02-11 21:16:24 +0100

    keyboard: implemented frame error detection and reserved 4 status bits for the scancode decoding routine (which has yet to be implemented)
    Isn't it nice how we can fit all the data needed by the keyboard driver
    into two bytes? This means we can just stuff it all into the small unused
    space behind the interrupt vector. This has the advantage that no other
    memory needs to be reserved for the keyboard driver. The only restriction
    is that the interrupt vector table has to be in RAM, which is a good idea
    anyways. Another restriction is that the keyboard driver cannot run in ADL
    mode. This doesn't matter. There may be some uses for ADL mode but the
    keyboard driver is certainly not one of them.

--- src/keyboard.asm
+++ src/keyboard.asm
@@ -49,46 +49,116 @@ DEFC PA_ALT0 = $A6
 	; B: status; C: incoming byte
 	LD	BC, (INTERRUPT_TABLE + KEYBOARD_ISR_DATA)
 
-	; the status byte has to be interpreted like this:
-	; $0B: getting parity bit
-	; $0A: waiting for stop bit
-	; $09: waiting for start bit <-default
-	; $08..$01: getting data bits
+	; the lower nibble of the status byte
+	; has to be interpreted like this:
+	; $B: getting parity bit
+	; $A: waiting for stop bit
+	; $9: waiting for start bit <-default
+	; $8..$1: getting data bits
 
-	LD	A, $08
-	CP	A, B
-	; carry means that status greater than $08
-	JR	C, keyboard_isr_get_frame_bit
+	LD	A, B
+	AND	A, $0F
+	CP	A, $09
+	; if status is greater than or equal to $9
+	JR	NC, keyboard_isr_get_frame_bit
 
+.keyboard_isr_get_data_bit
 	; get data bit
+	; C gets copied to A to make place for the TSTIO argument in C
+	; we need the data byte in A anyways, due to the use of RAA below
 	LD	A, C
 	LD	C, PA_DR
 	; eZ80 instruction: TSTIO $01
 	; note that this clears the carry flag
 	DEFB	$ED, $74, $01
-	JR	Z, keyboard_isr_data_bit_low
+	JR	Z, keyboard_isr_get_data_bit_low
+.keyboard_isr_get_data_bit_high
 	SCF
-.keyboard_isr_data_bit_low
+.keyboard_isr_get_data_bit_low
 	RRA
-	LD	C, A
-	;JR	keyboard_isr_next_status
+	JR	keyboard_isr_next_status
 
 .keyboard_isr_get_frame_bit
-	; ... frame bits are currently ignored and not validated
+	LD	A, B
+	AND	A, $0F
+	CP	A, $0A
+	; save data byte in A and set up C for the TSTIO coming later
+	; note that LD does not affect the flags set by CP above
+	LD	A, C
+	LD	C, PA_DR
+	; if status is equal to $A ($A = stop bit)
+	JR	Z, keyboard_isr_get_stop_bit
+	; if status is greater than $A ($B = parity bit)
+	JR	NC, keyboard_isr_get_parity_bit
+	; if status is less than $A ($9 = start bit)
+.keyboard_isr_get_start_bit
+	; eZ80 instruction: TSTIO $01
+	DEFB	$ED, $74, $01
+	; detect frame error (start bit should be 0)
+	JR	NZ, keyboard_isr_frame_error
+	JR	keyboard_isr_next_status
+.keyboard_isr_get_parity_bit
+	; get parity of data byte
+	; note that the data byte is already in A
+	OR	A, A
+	; if the parity of the data byte is odd,
+	; the parity bit should be 0, just like a start bit
+	JP	PO, keyboard_isr_get_start_bit
+	; otherwise we fall through to checking for a 1 (like a stop bit)
+.keyboard_isr_get_stop_bit
+	; eZ80 instruction: TSTIO $01
+	DEFB	$ED, $74, $01
+	; detect frame error (stop bit should be 1)
+	JR	Z, keyboard_isr_frame_error
+	;JR	keyboard_isr_next status
 
 .keyboard_isr_next_status
-	DJNZ	keyboard_isr_end
-	LD	B, $0B
-
-.keyboard_isr_end
+	; get data byte back into C
+	; note that both the data and frame bit routines put it in A
+	LD	C, A
+	; test if lower nibble of B equals zero
+	DEC	B
+	LD	A, B
+	; eZ80 instruction: TST	A, $0F
+	DEFB	$ED, $64, $0F
+	; if not, we're done here
+	JR	NZ, keyboard_isr_end_nowrap
+	; if it is zero, wrap around
+	; set lower nibble of B (status) to $B
+	; note that this marks the end of the data byte receive
+	; but not the end of the frame (parity and stop bit are following)
+	AND	A, $F0
+	OR	A, $0B
+	LD	B, A
+	; we can just skip the code below, because status is always $B here
+	JR	keyboard_isr_return
+.keyboard_isr_end_nowrap
+	; check if we are at the beginning again
+	; note that A still has a copy of B here
+	; if yes, process the received scancode
+	AND	A, $0F
+	CP	A, $09
+	JR	Z, keyboard_isr_scancode_received
+.keyboard_isr_return
 	; save data and status
 	LD	(INTERRUPT_TABLE + KEYBOARD_ISR_DATA), BC
 	; clear interrupt flag
 	LD	A, $02
 	; eZ80 instruction: OUT0 (PA_ALT0), A
 	DEFB	$ED, $39, PA_ALT0
-	; restore original state
+	; restore original state and return
 	POP	BC
 	POP	AF
 	EI
 	RETI
+
+.keyboard_isr_frame_error
+	; if we end up here, just start over waiting for a start bit
+	; this also clears the upper status bits, which is OK
+	LD	B, $09
+	JR	keyboard_isr_return
+
+.keyboard_isr_scancode_received
+	; process a received scancode
+	LD	B, $F9; <- just a test...
+	JR	keyboard_isr_return
